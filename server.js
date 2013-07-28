2cc0fd5e287cb1baaee85df5b635e6ee38813195
laukik <laukiknirgudkar@gmail.com>
2013-07-28 20:07:40 +0530

    server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var http = require('http');
 var path = require('path');
 var app = express();
-var spawn = require('child_process').spawn;
+var exec = require('child_process').exec;
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser({ keepExtensions: true,uploadDir: __dirname + '/upload'}));
@@ -16,15 +16,17 @@ app.post('/upload', function(req, res) {
  	var val = [];
  	val[0] = req.files.image.path;
  	console.log(val);
- 	var result = spawn('./upload/compile',val);
- 	console.log('sssdddddddddddddddd');
- 	result.stdout.on('data',function(data){
- 		console.log('next to execute');
- 		res.sendfile('index.html');
- 	});
- 	result.stderr.on('data',function(data){
- 		console.log('compilation error');
-
- 	});
+ 	var result = exec(__dirname + '/upload/compile.sh '+ val[0],function (error, stdout, stderr) {
+    	if(stderr){
+    		console.log('galat h');
+    		res.send(404);
+    	}else{
+    		console.log('sahi h');
+    		res.send(200);
+    	}
+    	if (error !== null) {
+      		console.log('exec error: ' + error);
+    	}
+	});
 });
 app.listen(8080);
